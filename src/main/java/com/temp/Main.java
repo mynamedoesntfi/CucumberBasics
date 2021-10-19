ce57bc84e928f4d1de0d214bae428741a88b236e
Nandan Bhat <48284547+mynamedoesntfi@users.noreply.github.com>
2021-10-19 16:56:11 +0530

    Initial commit

---
+++ src/main/java/com/temp/Main.java
@@ -0,0 +1,9 @@
+package com.temp;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/Steps/LoginStep.java
@@ -0,0 +1,23 @@
+package Steps;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class LoginStep {
+    @Given("^I navigate to the login page$")
+    public void iNavigateToTheLoginPage() {
+    }
+
+    @And("^I enter the username as admin and password as admin$")
+    public void iEnterTheUsernameAsAdminAndPasswordAsAdmin() {
+    }
+
+    @And("^I click login button$")
+    public void iClickLoginButton() {
+    }
+
+    @Then("^I should see the userform page$")
+    public void iShouldSeeTheUserformPage() {
+    }
+}
